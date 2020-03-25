610978bfc7f7a949e61ae29d23450ba04be4cd51
ripee <ripee.dev@gmail.com>
2020-03-26 00:06:58 +0100

    trlte-common: Remove F2FS

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,7 +161,6 @@ BOARD_PROVIDES_LIBRIL := true
 # Recovery
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
 LZMA_RAMDISK_TARGETS := recovery
 
 # Sensors
